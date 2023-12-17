0f2771b445707f8422daaa9cbf3a796e66a66ef4
pragatisaikia <pragatisaikia2003@gmail.com>
2023-12-17 18:07:56 +0530

    two initial questions

---
+++ binary_search/binary.java
@@ -0,0 +1,26 @@
+package binary_search;
+
+public class binary {
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,20,54,99};
+        int target = -2;
+        int result = binarySearch(arr, target);
+        System.out.println("Result : " + result);
+    }
+
+    public static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }else if (target > arr[mid]) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ binary_search/floor_ceiling.java
@@ -0,0 +1,45 @@
+package binary_search;
+
+public class floor_ceiling {
+    public static void main(String[] args) {
+        int[] arr = {2,3,6,12,16,17,20};
+        int target = 15;
+        int ceil = ceiling(arr, target);
+        int flr = floor(arr, target);
+        System.out.println("Ceiling of the number in the array : " + ceil);
+        System.out.println("Floor of the number in the array : " + flr);
+    }
+
+    public static int ceiling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            }else if (target > arr[mid]) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return arr[start];
+    }
+    public static int floor(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return arr[mid];
+            }else if (target > arr[mid]) {
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return arr[end];
+    }
+    }
+
+
