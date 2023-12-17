c3c6e98c281c6c53abe7877d515bfbc85a8fc82e
pragatisaikia <pragatisaikia2003@gmail.com>
2023-12-17 19:49:00 +0530

    infiniteArray

---
+++ binary_search/infiniteArray.java
@@ -0,0 +1,34 @@
+package binary_search;
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+public class infiniteArray {
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170 };
+        int target = 10;
+        System.out.println(ans(arr, target));
+    }
+
+    public static int ans(int arr[], int target) {
+        int start = 0;
+        int end = 1;
+        while (target > arr[end]) {
+            int temp = end + 1;
+            end = (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+    }
+
+    public static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
