459a69f3b38c53179329f9b0af137ffb1b762b87
pragatisaikia <pragatisaikia2003@gmail.com>
2023-12-19 12:35:52 +0530

    peak index

---
+++ binary_search/peak_mountain.java
@@ -0,0 +1,22 @@
+
+//852 & 162 : Peak Index in a Mountain Array : Medium
+public class peak_mountain {
+    public static void main(String[] args) {
+        int arr[] = {0,2,1,0};
+        int res = peakIndexInMountainArray(arr);
+        System.out.println(res);
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start; //here we are returning the index of the peak element in the array
+    }
+}
